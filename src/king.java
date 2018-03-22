b25aec86c7aa88221f831592371a7647ca94f11d
teekingtv <37657628+teekingtv@users.noreply.github.com>
2018-03-22 11:50:11 +0100

    Create king.java

---
+++ src/king.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+class king {
+
+	public static void main(String[] args) {
+		
+		tee tutor = new tee();
+		
+		tutor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tutor.setSize(600,100);
+		tutor.setVisible(true);
+		
+	}
+
+}
